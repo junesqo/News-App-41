171e20258492d7415999e5e0f480f420873b6959
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2022-03-19 17:18:43 +0600

    3rd lesson finished (setText bug in bind method in BoardAdapter)

---
+++ app/src/main/java/com/example/newsapp41/BoardAdapter.java
@@ -0,0 +1,66 @@
+package com.example.newsapp41;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.newsapp41.models.Board;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.ViewHolder> {
+
+    private ArrayList<Board> list;
+
+    public BoardAdapter() {
+        list = new ArrayList<>();
+        list.add(new Board("Салам"));
+        list.add(new Board("Привет"));
+        list.add(new Board("Hello"));
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.pager_board, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView textTitle;
+        private Button btnStart;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textTitle = itemView.findViewById(R.id.textTitle);
+            btnStart = itemView.findViewById(R.id.btnStart);
+        }
+
+        public void bind(int position) {
+            Board board = list.get(position);
+            textTitle.setText(board.getTitle());
+            if (position == list.size()-1) {
+                btnStart.setVisibility(View.VISIBLE);
+            } else {
+                btnStart.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+}
