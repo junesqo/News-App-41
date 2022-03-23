699b9d135beff9c516b76afdde43f8f024560b9a
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2022-03-23 21:53:09 +0600

    hw4: added edit text listener to shared preferences

--- app/src/main/java/com/example/newsapp41/Prefs.java
+++ app/src/main/java/com/example/newsapp41/Prefs.java
@@ -2,6 +2,8 @@ package com.example.newsapp41;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.util.Log;
 
 public class Prefs {
 
@@ -19,5 +21,21 @@ public class Prefs {
         return preferences.getBoolean("isBoardShown", false);
     }
 
+    public void saveImageUri(String uriString) {
+        preferences.edit().putString("profileImage", uriString).apply();
+        Log.e("Prefs", uriString);
+    }
 
+    public String getImageUri(){
+        return preferences.getString("profileImage", null );
+    }
+
+    public void saveProfileEditText(String etText){
+        preferences.edit().putString("profileET", etText).apply();
+        Log.e("Prefs", etText);
+    }
+
+    public String getProfileEditText(){
+        return preferences.getString("profileET", "");
+    }
 }
--- app/src/main/java/com/example/newsapp41/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/newsapp41/ui/profile/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.example.newsapp41.ui.profile;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -12,10 +13,14 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.newsapp41.Prefs;
 import com.example.newsapp41.R;
 import com.example.newsapp41.databinding.FragmentHomeBinding;
 import com.example.newsapp41.databinding.FragmentProfileBinding;
@@ -23,27 +28,60 @@ import com.example.newsapp41.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
 
+
     private FragmentProfileBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentProfileBinding.inflate(inflater, container, false);
+//        binding.imageView.setImageURI(Uri.parse(prefs.getImageUri()));
         return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        Prefs prefs = new Prefs(requireContext());
+        binding.profileEditText.setText(prefs.getProfileEditText());
         binding.imageView.setOnClickListener(view1 -> mGetContent.launch("image/*"));
+//        Prefs prefs = new Prefs(requireContext());
+//        binding.imageView.setImageURI(Uri.parse(prefs.getImageUri()));
+//        SharedPreferences settings = requireActivity().getSharedPreferences("key1", 0);
+//        String imageUriString = settings.getString("imageURI", null);
+//        Uri imageUri = Uri.parse(imageUriString);
+//        binding.imageView.setImageURI(imageUri);
+
+        binding.profileEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                prefs.saveProfileEditText(binding.profileEditText.getText().toString());
+            }
+        });
     }
 
     ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
         @Override
         public void onActivityResult(Uri result) {
+            Prefs prefs = new Prefs(requireContext());
             if (result != null) {
                 binding.imageView.setImageURI(result);
+
+                prefs.saveImageUri(result.toString());
             }
         }
     });
+
+
 }
